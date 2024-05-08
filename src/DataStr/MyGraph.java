06cf1655574a56c546f76e8dd1422f907b35478d
Markuss Puhovs <s22puhomark@vea.lv>
2024-05-08 12:07:10 +0300

    mygraph

--- src/DataStr/MyGraph.java
+++ src/DataStr/MyGraph.java
@@ -2,9 +2,10 @@ package DataStr;
 
 import java.util.ArrayList;
 
+import DataStr.nodes.MyEdgeNode;
 import DataStr.nodes.MyVerticeNode;
 
-public class MyGraph {
+public class MyGraph<Ttype> {
 	
 	private MyVerticeNode[] vertices;
 	private final int GRAPH_DEFAULT_SIZE = 10;
@@ -47,5 +48,79 @@ public class MyGraph {
 		size = newSize;
 	}
 	
-
+	//TODO
+	//pievienot jaunu virsotni grafa
+	public void addVertice(Ttype element) throws Exception {
+		if (element == null) throw new Exception("No elements");
+		if(isFull()) resize();
+		
+		//parbaude ka virsotne neeksiste
+		if(addVerticeHelp(element) != -1) {
+			throw new Exception("This vertice is already in graph");
+			
+		}else {
+			MyVerticeNode newNode = new MyVerticeNode<Ttype>(element);
+			vertices[counter++] = newNode;
+		}
+	}
+	
+	//atrast virsotni grafa un atgriezt ta index
+	private int addVerticeHelp(Ttype element) {
+		
+		for(int i = 0; i < counter; i++) {
+			if(vertices[i].getElement().equals(element)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//pievienot celu no konkretas virsotnes uz kadu citu virsotni
+	public void addEdge(Ttype verticeFrom, Ttype verticeTo, float weight) throws Exception {
+		if(verticeFrom == null || verticeTo == null || weight<=0 || weight >= 41000)
+			throw new Exception("There is a problem with params");
+		
+		if(verticeFrom.equals(verticeTo))
+			throw new Exception("It is not possible to create an edge to the same vertice");
+		
+		int indexFrom = addVerticeHelp(verticeFrom);
+		int indexTo = addVerticeHelp(verticeTo);
+		
+		if(indexFrom == -1 || indexTo == -1)
+			throw new Exception("One or both vertices are not in the graph");
+		
+		//TODO pārbaude, vai tads ceļs starp minetjaām virsotnēm jau neekesistē
+		MyEdgeNode newEdgeNode = new MyEdgeNode(indexTo, weight);
+		
+		//sis būs pirmais ceļa bloks sai virsotnei
+		if(vertices[indexFrom].getFirstEdgeNode() == null)
+		{
+			vertices[indexFrom].setFirstEdgeNode(newEdgeNode);
+		}
+		else
+		{
+			MyEdgeNode firstEdgeNode = vertices[indexFrom].getFirstEdgeNode();
+			newEdgeNode.setNext(firstEdgeNode);
+			firstEdgeNode.setPrevious(newEdgeNode);
+			vertices[indexFrom].setFirstEdgeNode(newEdgeNode);
+			
+		}
+	}
+	
+	//izprintet visas virsotnes ar piesaistitajiem celiem
+	public void print() throws Exception {
+		if(isEmpty()) throw new Exception("Graph is empty");
+		
+		for(int i = 0; i < counter; i++) {
+			System.out.print(vertices[i].getElement() + " ---> ");
+			MyEdgeNode tempEdge = vertices[i].getFirstEdgeNode();
+			while(tempEdge != null) {
+				System.out.print(vertices[tempEdge.getIndexOfNeighbour()].getElement() + " " + tempEdge.getWeight() + " km; ");
+				tempEdge = tempEdge.getNext();
+			}
+			System.out.println();
+		}
+	}
+	
+	//MainService izveidot karti ar vismaz 4 pilsetam un 6 celiem
 }
--- src/Service/MainService.java
+++ src/Service/MainService.java
@@ -1,10 +1,30 @@
 package Service;
 
+import DataStr.MyGraph;
+
 public class MainService {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyGraph<String> map = new MyGraph<String>();
+		try {
+			map.addVertice("Ventspils");
+			map.addVertice("Rīga");
+			map.addVertice("Kuldīga");
+			map.addVertice("Talsi");
+			map.addVertice("Tukums");
+			
+			map.addEdge("Ventspils", "Rīga", 189f);
+			map.addEdge("Ventspils", "Kuldīga", 56.49f);
+			map.addEdge("Ventspils", "Talsi", 64f);
+			map.addEdge("Tukums", "Rīga", 58f);
+			map.addEdge("Rīga", "Kuldīga", 148.88f);
+			
+			map.print();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
